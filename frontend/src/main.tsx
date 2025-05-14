717d47ed10efb9afad74af4ce3c41bc9a254469d
akhjunaid0047 <akhjunaid.047+github@gmail.com>
2025-05-14 21:38:12 +0530

    baseURL change

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,26 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
-import axios from 'axios';
-import { Toaster } from "react-hot-toast"
-axios.defaults.baseURL = 'http://localhost:3000/api/v1/';
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
+axios.defaults.baseURL = "https://mernchatbot.onrender.com/api/v1/";
 axios.defaults.withCredentials = true;
 const theme = createTheme({
-  typography: { fontFamily: "Robot Slab,serif", allVariants: { color: "white" } },
+  typography: {
+    fontFamily: "Robot Slab,serif",
+    allVariants: { color: "white" },
+  },
 });
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
-          <Toaster position='top-center' />
+          <Toaster position="top-center" />
           <App />
         </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>
-  </StrictMode>,
+  </StrictMode>
 );
